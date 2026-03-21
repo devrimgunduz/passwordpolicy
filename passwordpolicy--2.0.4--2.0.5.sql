f85818330f7f21dae3918ccc5bec3aa9e20fa57a
Francisco Miguel Biete Banon <fbiete@gmail.com>
2026-03-21 11:18:49 +0000

    chores(release) 2.0.5

---
+++ passwordpolicy--2.0.4--2.0.5.sql
@@ -0,0 +1,6 @@
+/* passwordpolicy/passwordpolicy--2.0.4--2.0.5.sql */
+
+-- complain if script is sourced in psql
+\echo Use "ALTER EXTENSION passwordpolicy UPDATE TO '2.0.5'" to load this file. \quit
+
+-- No changes to the database schema, just some code changes in the C code.
