c7ff900a80805883a9a4039652874ba9f2f4328e
Francisco Miguel Biete Banon <fbiete@gmail.com>
2026-04-05 20:06:46 +0100

    Copyright date missing

--- passwordpolicy_check.c
+++ passwordpolicy_check.c
@@ -2,7 +2,7 @@
  *
  * passwordpolicy.c
  *
- * Copyright (c) 2023, Francisco Miguel Biete Banon
+ * Copyright (c) 2023-2026, Francisco Miguel Biete Banon
  * Copyright (c) 2018-2023, indrajit
  * Copyright (c) 2009-2017, PostgreSQL Global Development Group
  * Author: Laurenz Albe <laurenz.albe@wien.gv.at>
